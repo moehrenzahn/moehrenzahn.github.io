01167e34ed8ae05ff55aad584557b4d73f0134d7
Max Melzer <max.melzer@moehrenzahn.de>
2019-12-27 15:42:29 +0100

    Update site

---
+++ makefile
@@ -0,0 +1,10 @@
+
+preview:
+	(sleep 6 && open http://127.0.0.1:4000/) &
+	bundle exec jekyll serve
+update:
+	git add .
+	git commit -m "Update site"
+	git push origin master
+new:
+	./scripts/new-post.php $$(pbpaste)
---
+++ scripts/new-post.php
@@ -0,0 +1,66 @@
+#!/usr/bin/php
+<?php
+print_r($argv);
+$url = $argv[1] ?? '';
+
+$date = (new DateTime())->format('Y-m-d');
+$title = 'Draft';
+
+if ($url) {
+    echo "Parsing URL '$url' …" . PHP_EOL;
+    $content = file_get_contents($url);
+    if ($content) {
+        $title = getTitleFromHtml($content);
+        echo "Using title '$title' …" . PHP_EOL;
+    }
+}
+$filePath = implode('/', [
+    __DIR__,
+    '..',
+    '_posts',
+    prepareFilename($date, $title),
+]);
+
+$templateParts = [
+    '---',
+    'layout: post',
+    "title: $title",
+    'categories: []'
+];
+
+if ($url) {
+    $templateParts[] = "link: $url";
+}
+
+$templateParts[] = '---';
+
+echo "Creating file '$filePath' …" . PHP_EOL;
+
+file_put_contents(
+    $filePath,
+    implode(PHP_EOL, $templateParts) . PHP_EOL . PHP_EOL
+);
+
+exec("open $filePath");
+
+function prepareFilename(string $date, string $title): string
+{
+    $title = strtolower($title);
+    $title = str_replace([' ', '–', '—'], '-', $title);
+
+    while (strpos($title, '--') !== false) {
+        $title = str_replace('--', '-', $title);
+    }
+    
+    return "$date-$title.md";
+}
+
+function getTitleFromHtml(string $html): string
+{
+    $dom = new DOMDocument();
+    @$dom->loadHTML($html);
+    $title = $dom->getElementsByTagName('title')[0] ?? '';
+    $title = trim($title->textContent);
+
+    return $title;
+}
