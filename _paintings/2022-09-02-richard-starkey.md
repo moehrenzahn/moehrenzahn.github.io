2edced2ef5d67679ce84ab34c3558f3d0a42ec7d
Max Melzer <max.melzer@moehrenzahn.de>
2022-09-02 23:59:25 +0200

    Add new painting, greatly improve progress gif generation script

---
+++ _paintings/2022-09-02-richard-starkey.md
@@ -0,0 +1,14 @@
+---
+title: "Richard Starkey"
+file: /paintings/2022-09-02-richard-starkey.jpg
+date: 2022-08-31
+size: 20×20cm
+materials: Acrylics on canvas board
+featured: no
+---
+
+A small portrait of Sir Richard Starkey, a.k.a. Ringo Starr from The Beatles. I used [this photograph](https://m.imdb.com/title/tt0064622/mediaviewer/rm2881208321) as a reference.
+
+Here is an animation visualising my painting process:
+
+![Painting process animation](/paintings/richard-starkey-process.gif)
--- _scripts/progress-gif.sh
+++ _scripts/progress-gif.sh
@@ -1,7 +1,45 @@
 #!/usr/bin/env sh
 set -ex
-convert -resize x500 \
-        -delay 20 \
-        -morph 10 \
+
+magick convert \
+        -size 500x500 \
+        xc:\#def \
+        ~/Desktop/0.jpg
+
+images=($(ls ~/Desktop/*.jpg))
+for image in "${images[@]}"; do
+    cp "${image}" "${image}_2.jpg"
+    cp "${image}" "${image}_3.jpg"
+done
+
+magick \
+        ~/Desktop/*.jpg \
+        -resize x300 \
+        -morph 5 \
+        -delay 5 \
         -loop 0 \
-        ~/Desktop/*.jpg ~/Desktop/animation.gif
+        ~/Desktop/animation.gif
+
+magick \
+        ~/Desktop/*.jpg \
+        -resize x1000 \
+        -morph 10 \
+        -delay 0 \
+        -loop 0 \
+        ~/Desktop/animation_large.gif
+
+rm ~/Desktop/0.jpg
+for image in "${images[@]}"; do
+    rm "${image}_2.jpg"
+    rm "${image}_3.jpg"
+done
+
+magick \
+        ~/Desktop/animation_large.gif \
+        ~/Desktop/animation_fast.mp4
+
+ffmpeg -i ~/Desktop/animation_fast.mp4 -filter:v "setpts=2.0*PTS" ~/Desktop/animation.mp4 -y
+
+/Applications/ImageOptim.app/Contents/MacOS/ImageOptim ~/Desktop/animation.gif
+rm ~/Desktop/animation_fast.mp4
+rm ~/Desktop/animation_large.gif
